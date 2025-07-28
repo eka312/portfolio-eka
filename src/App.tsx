1b628d7b8858934c6b463a59a46207ea549d2ab2
eka312 <fue312@gmail.com>
2025-07-28 10:05:34 +0700

    membuat protect route

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PreLoader } from './komponen/PreLoader.tsx';
 import Dashboard from "./pages/Dashboard.tsx";
 import Proyek from './pages/Proyek.tsx';
 import HalDetail from "./pages/HalDetail.tsx";
-
+import ProtectedRoute from './router/ProtectedRoute.tsx';
 
 function App() {
   const showPreloader = location.pathname === "/";
@@ -17,8 +17,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/proyek" element={<Proyek/>} />
+        <Route path="/dashboard" element={ <ProtectedRoute><Dashboard/></ProtectedRoute>} />
+        <Route path="/proyek" element={ <ProtectedRoute><Proyek/></ProtectedRoute>} />
         <Route path="/detail/:slug" element={<HalDetail />} />
       </Routes>
     </>
